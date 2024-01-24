8a7127ac26617e0df82ad07bd3d947329df80338
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2024-01-24 20:37:21 -0300

    Initial commit

---
+++ test/aws-sns-cronjob.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsSnsCronjob from '../lib/aws-sns-cronjob-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-sns-cronjob-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsSnsCronjob.AwsSnsCronjobStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
