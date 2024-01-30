b07a30c411ff62888208336cdf8339cbf82243c0
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2024-01-29 23:24:03 -0300

    Cron config

--- lib/aws-sns-cronjob-stack.ts
+++ lib/aws-sns-cronjob-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
+import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
+import { Topic } from 'aws-cdk-lib/aws-sns';
+import { EmailSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
+import { SnsTopic } from 'aws-cdk-lib/aws-events-targets';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsSnsCronjobStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const rule = new Rule(this, 'rule', {
+      schedule: Schedule.expression('cron(30 18 * * MON-FRI *)')
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsSnsCronjobQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const topic = new Topic(this, 'topic', {
+      displayName: 'My Topic'
+    });
+
+    topic.addSubscription(new EmailSubscription('email'));
+
+    rule.addTarget(new SnsTopic(topic));
   }
 }
